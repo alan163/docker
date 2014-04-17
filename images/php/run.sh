6bf6e152c36f088b96f4918a63c48976ae5c04f9
wangtao04 <wangtao04@baidu.com>
2014-04-18 01:45:54 +0800

    mod

--- images/php/run.sh
+++ images/php/run.sh
@@ -1,1 +1,8 @@
-install_dir="/storm"
+#!/bin/bash
+docker_dir="/docker/images/php"
+cp $docker_dir/conf/php.ini /php/etc/
+cp $docker_dir/conf/php-fpm.conf /php/etc/
+cp $docker_dir/conf/nginx/* /nginx/conf/
+/php/sbin/php-fpm
+/nginx/sbin/nginx
+tail -f /nginx/log/*

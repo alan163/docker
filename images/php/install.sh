ca37d73be8831c233a4e4b71f6820955b2fb98cb
wangtao <wangtao@wangtaodeMac-mini.local>
2014-06-16 14:51:36 +0800

    mod

--- images/php/install.sh
+++ images/php/install.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 set -e
-PHP_VERSION=5.3.5
-NGINX_VERSION=0.8.55
+PHP_VERSION=5.4.28
+NGINX_VERSION=1.4.5
 
 #groupadd  work && useradd work -m -s /bin/bash -d /home/work -g work
 #rpm -Uvh http://epel.mirror.net.in/epel/6/i386/epel-release-6-8.noarch.rpm
@@ -9,10 +9,10 @@ NGINX_VERSION=0.8.55
 #yum -y install gcc pcre pcre-devel gcc-c++ autoconf libxml2 libxml2-devel zlib zlib-devel glibc libjpeg libjpeg-devel libpng libpng-devel glibc-devel glib2 glib2-devel ncurses ncurses-devel curl curl-devel e2fsprogs e2fsprogs-devel openssl openssl-devel openldap openldap-devel nss_ldap openldap-clients openldap-servers libxml2-devel libxslt-devel   gd openssh-clients mysql-devel GeoIP GeoIP-devel vi vim perl-devel gd-devel
 su work - && cd /home/work/
 
-wget http://fantasymobile-test1.socialgamenet.com/src/nginx-${NGINX_VERSION}.tar.gz && \
+wget http://nginx.org/download/nginx-1.4.5.tar.gz \
 tar zxf nginx-${NGINX_VERSION}.tar.gz && \
 cd nginx-${NGINX_VERSION} &&  \
-./configure --prefix=/home/work/webserver/  --error-log-path=/home/work/logs/error.log --http-log-path=/home/work/logs/access.log --http-client-body-temp-path=/home/work/webserver/tmp/client_body --http-proxy-temp-path=/home/work/webserver/tmp/proxy --http-fastcgi-temp-path=/home/work/webserver/tmp/fastcgi --http-uwsgi-temp-path=/home/work/webserver/tmp/uwsgi --http-scgi-temp-path=/home/work/webserver/tmp/scgi --pid-path=/home/work/var/nginx.pid --lock-path=//home/work/var/nginx.lock --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_xslt_module --with-http_image_filter_module --with-http_geoip_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_stub_status_module --with-http_perl_module --with-mail --with-file-aio --with-mail_ssl_module --with-ipv6 && \
+./configure --prefix=/home/work/webserver/  --error-log-path=/home/work/logs/nginx/error.log --http-log-path=/home/work/logs/nginx/access.log --http-client-body-temp-path=/home/work/webserver/tmp/client_body --http-proxy-temp-path=/home/work/webserver/tmp/proxy --http-fastcgi-temp-path=/home/work/webserver/tmp/fastcgi --http-uwsgi-temp-path=/home/work/webserver/tmp/uwsgi --http-scgi-temp-path=/home/work/webserver/tmp/scgi --pid-path=/home/work/var/nginx.pid --lock-path=//home/work/var/nginx.lock --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_xslt_module --with-http_image_filter_module --with-http_geoip_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_stub_status_module --with-http_perl_module --with-mail --with-file-aio --with-mail_ssl_module --with-ipv6 && \
 make && make install
 
 wget http://fantasymobile-test1.socialgamenet.com/src/php-${PHP_VERSION}.tar.gz && \
